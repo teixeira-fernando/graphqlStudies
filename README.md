0b90582d58d72b1ec9ccc2a62a3fc33bdf1c2fb7
Fernando Teixeira <fernandoadt1@gmail.com>
2021-03-31 18:48:14 -0300

    Migrated to Typescript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Studies of GraphQL, Node.js, SuperTest and Jest
+
+To compile Typescript using Visual Studio Code, use: Ctrl + Shift + B
---
+++ src/schema.ts
@@ -0,0 +1,27 @@
+/* src/schema.js */
+
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+    type Query {
+        users: [User!]!,
+        user(id: Int!): User!
+    }
+ 
+    type User {
+        id: ID!
+        name: String!
+        email: String
+        posts: [Post!]
+    }
+ 
+    type Post {
+        id: ID!
+        title: String!
+        published: Boolean!
+        link: String
+        author: User!
+    }
+`);
+
+module.exports = schema;
